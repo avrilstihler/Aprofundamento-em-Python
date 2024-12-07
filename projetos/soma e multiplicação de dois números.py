ef057187ccf6335954ab2392dfbe1400e38568ea
avrilstihler <avrilstihlers@gmail.com>
2024-12-07 20:16:31 -0300

    Criado soma e multiplicação de dois números.py

---
+++ projetos/soma e multiplicação de dois números.py
@@ -0,0 +1,13 @@
+# Solicita ao usuário para inserir dois números
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+# Calcula a soma
+resultado_soma = num1 + num2
+
+# Calcula a multiplicação
+resultado_mult = num1 * num2
+
+# Exibe o resultado
+print('O resultado da soma entre os números', num1, 'e', num2, 'é igual a:', resultado_soma)
+print('O resultado da multiplicação entre os números', num1, 'e', num2, 'é igual a:', resultado_mult)
