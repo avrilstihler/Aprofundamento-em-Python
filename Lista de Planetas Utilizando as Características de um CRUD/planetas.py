5aa370879f695b7b187a67eb740094773182a12b
avrilstihler <avrilstihlers@gmail.com>
2024-12-07 21:31:35 -0300

    Create planetas.py

---
+++ Lista de Planetas Utilizando as Características de um CRUD/planetas.py
@@ -0,0 +1,10 @@
+planetas = {
+  'Mercúrio': {'tamanho': 4879, 'distancia': 57.9, 'apelido': 'O Planeta Menino'},
+  'Vênus': {'tamanho': 12104, 'distancia': 108.2, 'apelido': 'Estrela d\'Alva'},
+  'Terra': {'tamanho': 12756, 'distancia': 149.6, 'apelido': 'O Planeta Azul'},
+  'Marte': {'tamanho': 6792, 'distancia': 227.9, 'apelido': 'Planeta Vermelho'},
+  'Júpiter': {'tamanho': 142984, 'distancia': 778.6, 'apelido': 'Gigante Gasoso'},
+  'Saturno': {'tamanho': 120536, 'distancia': 1433.5, 'apelido': 'Senhor dos Anéis'},
+  'Urano': {'tamanho': 51118, 'distancia': 2872.5, 'apelido': 'Gigante de Gelo'},
+  'Netuno': {'tamanho': 49528, 'distancia': 4504.3, 'apelido': 'Gigante de Gelo'}
+}
